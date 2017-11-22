6d65058e0bf09e83f6f1ad21c61560a7e10a60f6
LIYI99 <627387729@qq.com>
2017-11-22 01:54:59 -0800

    LIYI update

---
+++ gk_image_sdk_new.h
@@ -0,0 +1,225 @@
+#ifndef __GK_IMAGE_SDK_H__
+#define __GK_IMAGE_SDK_H__
+
+#include <stdint.h>
+#include <ptread.h>
+#include "gk_mouse_event.h"
+#include "object_pool.h"
+
+#ifdef __cplusplus
+    extern "C"{
+#endif
+
+typedef  enum{
+    OBJECT_BUTTION,
+    OBJECT_MENU,
+    OBJECT_LINE,
+    OBJECT_TXT_WIN,
+    OBJECT_NUMS,
+
+}WIN_TYPE_S;
+
+
+
+
+
+
+typedef struct window_node      window_node_t;
+
+typedef enum {
+    NOT_MOVE,
+    MOUSE_OFFET_MOVE,
+    MOUSE_LDOWN_MOVE,
+    MOUSE_LUP_MOVE,
+    MOUSE_RDOWN_MOVE,
+    MOUSE_RUP_MOVE,
+    MOVE_NUMS,
+}NODE_MOVE_ARRT;
+
+struct window_node{
+
+#define     MENU_LEVEL              5
+    char            node_id[MENU_LEVEL]; // ever level A -z sum 52 level 
+    window_node_t   *prev;
+    window_node_t   *next;
+    window_node_t   *s_head;
+    window_node_t   *s_end;
+    window_node_t   *f_node;
+    uint8_t         en_node;
+    uint8_t         check_node;
+    uint8_t         en_freshen;
+    uint8_t         en_submenu;
+
+#define         CLOSE_REFLECT       0
+#define         OFFSET_REFLECT      1
+#define         LDOWN_REFLECT       2
+#define         LUP_REFLECT         4
+#define         RDOWN_REFLECT       8
+#define         RUP_REFLECT         16
+    uint8_t         mouse_garp_reflect;
+    NODE_MOVE_ARRT  move_arrt;
+    WIN_TYPE_S      win_type;
+    void            *window;
+};
+
+
+typedef void  (*win_func)(void *data);
+
+
+
+typedef struct window_func{
+    
+    win_func        mouse_offset;
+    win_func        mouse_left_down;
+    win_func        mouse_left_up;
+    win_func        mouse_right_down;
+    win_func        mouse_right_up;
+    win_func        mouse_leave;
+
+}window_func_t;
+
+typedef struct window_node_button{
+   
+    window_node_t   *this_node; 
+    uint16_t        x;
+    uint16_t        y;
+    uint16_t        w;
+    uint16_t        h;
+    uint16_t        size;
+    uint32_t        color; 
+    char            *image_cache;
+    
+    window_func_t   func_set;
+    void            *user_data;
+
+}window_node_button_t;
+
+
+typedef struct window_node_menu{
+    window_node_t   *this_node;
+    uint16_t        x;
+    uint16_t        y;
+    uint16_t        w;
+    uint16_t        h;
+    uint16_t        size;
+    uint32_t        color; 
+    char            *image_cache;
+    window_func_t   func_set;
+    void            *user_data;
+}window_node_menu_t;
+
+typedef struct window_node_line{
+       
+    window_node_t   *this_node;
+    uint16_t        start_x;
+    uint16_t        start_y;
+    uint16_t        end_x;
+    uint16_t        end_y;
+    uint16_t        size;
+    uint32_t        color; 
+    char            *image_cache;
+    
+    window_func_t   func_set;
+    void            *user_data;
+}window_node_line_t;
+
+typedef struct window_node_text{
+       
+    window_node_t   *this_node;
+    uint16_t        x;
+    uint16_t        y;
+    uint16_t        size;
+    uint16_t        lens;
+    uint32_t        win_color;
+    uint32_t        text_color;
+    char            *text_cache;
+    
+    window_func_t   func_set;
+    void            *user_data;
+
+}window_node_text_t;
+
+typedef struct window_node_mouse{
+    
+    uint16_t        x_last;        //old
+    uint16_t        y_last;        //old
+    uint16_t        x;
+    uint16_t        y;
+    uint16_t        size;
+    uint32_t        color;
+    char            *image_cache0;
+    char            *image_cache1;
+    char            *image_cache2;
+    char            *save_cache;
+
+   
+
+}window_node_mouse_t;
+
+
+typedef struct  image_sdk_s{
+         
+    uint16_t        en;
+    uint16_t        scree_w;
+    uint16_t        scree_h;
+    uint16_t        color_fmt;
+    uint16_t        disp_fps;
+    
+    int             video_fd;
+    void            *mmap_p;
+
+    int             mouse_fd;
+    GK_MOUSE_DATA   mouse_new_data; 
+    
+    window_node     *root;
+       
+    object_pool_t   *window_node_pool;
+    object_pool_t   *object_pool;
+    
+    window_node_t   *check_node;
+    window_node_t   *last_check_node;
+    window_node_mouse_t *mouse;
+}image_sdk_t;
+
+
+
+
+void    Image_SDK_Init(void);
+void    Image_SDK_deInit(void);
+void    Image_SDK_Run(void);
+
+
+struct user_set_node_atrr{
+    char            node_id[MENU_LEVEL]; 
+    uint8_t         en_node;
+    uint8_t         en_freshen;
+    NODE_MOVE_ARRT  move_arrt;
+};
+
+
+
+int    Image_SDK_Create_Button( struct user_set_node_atrr attr,
+        window_node_button_t _button);
+int    Image_SDK_Create_Menu(struct user_set_node_atrr attr,
+        window_node_menu_t _menu);
+int    Image_SDK_Create_Line(struct user_set_node_atrr attr,
+        window_node_line_t _line);
+int    Image_SDK_Create_Text(struct user_set_node_atrr arrt,
+        window_node_text_t _text);
+
+int     Image_SDK_Set_Node_Move_Atrr(char *node_id,NODE_MOVE_ARRT _arrt);
+int     Image_SDK_Set_Node_En(char *node_id,uint8_t en);
+int     Image_SDK_Set_Node_En_Freshen(char *node_id,uint8_t en_freshen);
+
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
+#endif
