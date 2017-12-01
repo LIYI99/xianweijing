6e09bf2275c1a5f545f7058a4ac1e03f1162c623
LIYI99 <627387729@qq.com>
2017-11-30 22:23:17 -0800

    LIYIupdate

---
+++ xw_window_id_df.h
@@ -0,0 +1,99 @@
+#ifndef __XW_WINDOW_ID_DF_H__
+#define __XW_WINDOW_ID_DF_H__
+
+#define     XW_ROOT_WINDOW_ID   "A"
+
+//dedfine   main menu window
+#define XW_MAIN_WINDOW_ID       "Aa"
+
+
+//#define   snap recod set perview
+#define XW_SNAP_WINDOW_ID           "Aaz"
+#define XW_RECOD_WINDOW_ID          "Aay"
+#define XW_SET_WINDOW_ID            "Aax"
+#define XW_PERVIEW_WINDOW_ID        "Aaw"
+
+//deinfine line set button
+#define XW_LINE_SHOW_WINDOW_ID              "Aav"
+#define XW_LINE_CLOSE_WINDOW_ID             "Aau"
+#define XW_LINE_RESET_WINDOW_ID             "Aat"
+#define XW_LINE_SAVE_WINDOW_ID              "Aas"
+#define XW_LINE_CHECK_WINDOW_ID             "Aar"
+#define XW_LINE_ONLY_WINDOW_ID              "Aaq"
+#define XW_LINE_CHANGE_COLOR_WINDOW_ID      "Aap"
+#define XW_LINE_SET_SIZE_WINDOW_ID          "Aao"
+
+//define video advanced settings
+#define XW_VIDEO_ZOOZ_LESSEN_WINDOW_ID      "Aan"
+#define XW_VIDEO_ZOOZ_BOOST_WINDOW_ID       "Aam"
+#define XW_VIDEO_FILP_WINDOW_ID             "Aal"
+#define XW_VIDEO_MIRROR_WINDOW_ID           "Aak"
+#define XW_VIDEO_NIGHGT_WINDOW_ID           "Aaj"
+#define XW_VIDEO_DAY_WINDOW_ID              "Aai"
+#define XW_VIDEO_HDR_WINDOW_ID              "Aah"
+#define XW_VIDEO_UNLOCK_WINDOW_ID           "Aag"
+
+//define date text window id 
+#define XW_DATE_YEAR_WINDOW_ID              "Aaf"
+#define XW_DATE_MONN_WINDOW_ID              "Aae"
+#define XW_DATE_DAY_WINDOW_ID               "Aad"
+#define XW_DATE_HOUR_WINDOW_ID              "Aac"
+#define XW_DATE_MIN_WINDOW_ID               "Aab"
+#define XW_DATE_SEC_WINDOW_ID               "Aaa"
+#define XW_DATE_SET_UP_WINDOW_ID            "AaZ"
+#define XW_DATE_SET_DOWN_WINDOW_ID          "AaY"
+
+
+//define isp set window id
+#define XW_ISP_COLOR_TEMP_WINDOW_ID         "AaX"
+#define XW_ISP_COLOR_TEMP_TEXT_WINDOW_ID    "AaW"
+#define XW_ISP_BULE_WINDOW_ID               "AaV"
+#define XW_ISP_BULE_TEXT_WINDOW_ID          "AaU"
+#define XW_ISP_RED_TEXT_WINDOW_ID           "AaT"
+#define XW_ISP_DENOISE_WINDOW_ID            "AaS"
+#define XW_ISP_DENOISE_TEXT_WINDOW_ID       "AaR"
+#define XW_ISP_SHARPNESS_WINDOW_ID          "AaQ"
+#define XW_ISP_SHARPNESS_TEXT_WINDOW_ID     "AaP"
+#define XW_ISP_BRIGHTNESS_WINDOW_ID         "AaO"
+#define XW_ISP_BRIGHTNESS_TEXT_WINDOW_ID    "AaN"
+#define XW_ISP_SATURATION_WINDOW_ID         "AaM"
+#define XW_ISP_SATURATION_TEXT_WINDOW_ID    "AaL"
+#define XW_ISP_CONTRAST_WINDOW_ID           "AaK"
+#define XW_ISP_CONTRAST_TEXT_WINDOW_ID      "AaJ"
+#define XW_ISP_GAMMA_WINDOW_ID              "AaI"
+#define XW_ISP_GAMMA_TEXT_WINDOW_ID         "AaH"
+
+//
+#define XW_FILCKER_50H_WINDOW_ID            "AaG"
+#define XW_FILCKER_50H_WINDOW_ID            "AaF"
+#define XW_AUTO_WHITE_BALANCE_WINDOW_ID     "AaE"
+#define XW_AE_TARGET_WINDOW_ID              "AaD"
+#define XW_AUTO_EXPOUSURE_WINDOW_ID         "AaC"
+
+//line manager 
+#define XW_LINE_RARR_WINDOW_ID               "Ab"
+#define XW_LINE_H1_WINDOW_ID                "Aba"
+#define XW_LINE_H2_WINDOW_ID                "Abb"
+#define XW_LINE_H3_WINDOW_ID                "Abc"
+#define XW_LINE_H4_WINDOW_ID                "Abd"
+#define XW_LINE_H5_WINDOW_ID                "Abe"
+#define XW_LINE_H6_WINDOW_ID                "Abf"
+#define XW_LINE_H7_WINDOW_ID                "Abg"
+#define XW_LINE_H8_WINDOW_ID                "Abh"
+#define XW_LINE_H9_WINDOW_ID                "Abj"
+
+#define XW_LINE_W1_WINDOW_ID                "Abk"
+#define XW_LINE_W2_WINDOW_ID                "Abl"
+#define XW_LINE_W3_WINDOW_ID                "Abm"
+#define XW_LINE_W4_WINDOW_ID                "Abn"
+#define XW_LINE_W5_WINDOW_ID                "Abo"
+#define XW_LINE_W6_WINDOW_ID                "Abp"
+#define XW_LINE_W7_WINDOW_ID                "Abq"
+#define XW_LINE_W8_WINDOW_ID                "Abr"
+#define XW_LINE_W9_WINDOW_ID                "Abs"
+
+
+
+
+
+#endif
