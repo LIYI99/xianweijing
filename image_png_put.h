4f24ab8254be1e3a83e0e7eff4a84227cc7ad9d9
LIYI99 <627387729@qq.com>
2018-01-07 04:16:43 -0800

    LIYIUPDATE

--- image_png_put.h
+++ image_png_put.h
@@ -1,12 +1,24 @@
 #ifndef __IMAGE_PNG_PUT_H__
 #define __IMAGE_PNG_PUT_H__
 
+#include <stdint.h>
 
+
+#ifdef  __cplusplus
+    extern  "C"{
+#endif
+    
+    
 
 
 int  image_png_load_rgba_16bit(char *path,uint16_t *mem,uint32_t *h,uint32_t *w,uint8_t flags);
 
 
+
+
+#ifdef  __cplusplus
+}
+#endif
 
 
 #endif
